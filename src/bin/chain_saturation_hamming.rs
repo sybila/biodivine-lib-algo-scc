ea26f0a21ede4d41f7e6c4b95bf6c178151d9e28
Samuel Pastva <sam.pastva@gmail.com>
2024-09-10 17:59:06 +0200

    Add constant inlining and extra variants with trimming and hamming distance.

--- src/bin/chain_saturation_hamming.rs
+++ src/bin/chain_saturation_hamming.rs
@@ -8,11 +8,12 @@ fn main() {
     assert_eq!(args.len(), 2);
 
     let bn = BooleanNetwork::try_from_file(&args[1]).unwrap();
+    let bn = bn.inline_constants(true, true);
     let graph = SymbolicAsyncGraph::new(&bn).unwrap();
 
     println!("Loaded BN with {} variables.", bn.num_vars());
 
-    let mut scc_list = cejn::chain::chain_saturation(&graph).collect::<Vec<_>>();
+    let mut scc_list = cejn::chain::chain_saturation_hamming_heuristic(&graph).collect::<Vec<_>>();
     scc_list.sort_by_key(|it| it.exact_cardinality());
 
     let trivial = scc_list.iter().filter(|it| it.is_singleton()).count();
